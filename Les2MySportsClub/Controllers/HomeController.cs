0d83da4e1ad6333e2d9d5ce5af1092c05a68e804
Paul de Mast <pjf.demast@avans.nl>
2022-02-09 16:48:52 +0100

    Add project files.

---
+++ Les2MySportsClub/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Les2MySportsClub.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Les2MySportsClub.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Les2MySportsClub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Les2MySportsClub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
