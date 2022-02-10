f4859720773e8b95d71330e135b912a97989a713
Paul de Mast <pjf.demast@avans.nl>
2022-02-10 12:37:10 +0100

    add display templates foor Date and Time

--- Les2MySportsClub/Data/FakeData.cs
+++ Les2MySportsClub/Data/FakeData.cs
@@ -8,7 +8,7 @@ namespace Les2MySportsClub.Data
 
         public static List<Member> FakeMembers
             = new List<Member> {
-                new Member{ID= 1, Name = "Esther", StartMembership = new DateTime(2014, 10, 8)},
+                new Member{ID= 1, Name = "Marcel", StartMembership = new DateTime(2014, 10, 8)},
                 new Member{ID= 2, Name = "Anton", StartMembership = new DateTime(2018, 1, 5)},
                 new Member{ID= 3, Name = "Manon", StartMembership = new DateTime(2016, 6, 1)},
                 new Member{ID= 4, Name = "Joke", StartMembership = new DateTime(2019, 1, 10)},
@@ -23,20 +23,20 @@ namespace Les2MySportsClub.Data
 
         public static List<Workout> FakeWorkouts
             = new List<Workout> {
-                new Workout{ID= 1, Title = "Yin Yoga", Location = "Yoga studio", StartTime = today.NextDayAt(DayOfWeek.Monday, 10, 15), EndTime = today.NextDayAt(DayOfWeek.Monday, 11, 0), CapacityLeft = 35},
-                new Workout{ID= 2, Title = "Pilates", Location = "Yoga studio", StartTime = today.NextDayAt(DayOfWeek.Monday, 17, 0), EndTime = today.NextDayAt(DayOfWeek.Monday, 18, 0), CapacityLeft = 30},
-                new Workout{ID= 3, Title = "Hot Yoga", Location ="Yoga studio", StartTime = today.NextDayAt(DayOfWeek.Tuesday, 10, 15), EndTime = today.NextDayAt(DayOfWeek.Tuesday, 11, 15), CapacityLeft=35},
-                new Workout{ID= 4, Title = "Club Power", Location = "Room 1",StartTime = today.NextDayAt(DayOfWeek.Tuesday, 19, 15), EndTime = today.NextDayAt(DayOfWeek.Tuesday, 20, 15), CapacityLeft=30},
-                new Workout{ID= 5, Title = "XCO", Location = "Room 2",StartTime = today.NextDayAt(DayOfWeek.Wednesday, 9, 15), EndTime = today.NextDayAt(DayOfWeek.Wednesday, 10, 15), CapacityLeft=25},
-                new Workout{ID= 6, Title = "B&K Training", Location = "Boxing Area",StartTime = today.NextDayAt(DayOfWeek.Wednesday, 10, 15), EndTime = today.NextDayAt(DayOfWeek.Wednesday, 11, 15), CapacityLeft= 16},
-                new Workout{ID= 7, Title = "Callanetics", Location = "Room 1", StartTime = today.NextDayAt(DayOfWeek.Wednesday, 19, 15), EndTime = today.NextDayAt(DayOfWeek.Wednesday, 20, 0), CapacityLeft=35},
-                new Workout{ID= 8, Title = "Spinning", Location = "Room 4", StartTime = today.NextDayAt(DayOfWeek.Thursday, 10, 15), EndTime = today.NextDayAt(DayOfWeek.Thursday, 11, 15), CapacityLeft=18},
-                new Workout{ID= 9, Title = "Vinyasa Yoga", Location ="Yoga studio", StartTime = today.NextDayAt(DayOfWeek.Thursday, 17, 15), EndTime = today.NextDayAt(DayOfWeek.Thursday, 18, 15), CapacityLeft=30},
-                new Workout{ID= 10, Title = "TBW", Location = "Room 1", StartTime = today.NextDayAt(DayOfWeek.Friday, 10, 15), EndTime = today.NextDayAt(DayOfWeek.Friday, 11, 0), CapacityLeft=35},
-                new Workout{ID= 11, Title = "Shred and Burn", Location = "Room 2", StartTime = today.NextDayAt(DayOfWeek.Friday, 10, 30), EndTime = today.NextDayAt(DayOfWeek.Friday, 11, 15), CapacityLeft=12},
-                new Workout{ID= 12, Title = "Cycle Interval", Location = "Cycle Area", StartTime = today.NextDayAt(DayOfWeek.Friday, 18, 15), EndTime = today.NextDayAt(DayOfWeek.Friday, 19, 15), CapacityLeft=8},
-                new Workout{ID= 13, Title = "Spinning", Location = "Cycle Area", StartTime = today.NextDayAt(DayOfWeek.Saturday, 9, 15), EndTime = today.NextDayAt(DayOfWeek.Saturday, 10, 15), CapacityLeft=12},
-                new Workout{ID= 14, Title = "Cycle & Abs", Location = "Cycle Area", StartTime = today.NextDayAt(DayOfWeek.Sunday, 10, 15), EndTime = today.NextDayAt(DayOfWeek.Sunday, 11, 15), CapacityLeft=6},
+                new Workout{ID= 1, Instructor="Marcel", Title = "Yin Yoga", Location = "Yoga studio", StartTime = today.NextDayAt(DayOfWeek.Monday, 10, 15), EndTime = today.NextDayAt(DayOfWeek.Monday, 11, 0), CapacityLeft = 35},
+                new Workout{ID= 2, Instructor="Babs", Title = "Pilates", Location = "Yoga studio", StartTime = today.NextDayAt(DayOfWeek.Monday, 17, 0), EndTime = today.NextDayAt(DayOfWeek.Monday, 18, 0), CapacityLeft = 30},
+                new Workout{ID= 3, Instructor="Viviene", Title = "Hot Yoga", Location ="Yoga studio", StartTime = today.NextDayAt(DayOfWeek.Tuesday, 10, 15), EndTime = today.NextDayAt(DayOfWeek.Tuesday, 11, 15), CapacityLeft=35},
+                new Workout{ID= 4, Instructor="Laura", Title = "Club Power", Location = "Room 1",StartTime = today.NextDayAt(DayOfWeek.Tuesday, 19, 15), EndTime = today.NextDayAt(DayOfWeek.Tuesday, 20, 15), CapacityLeft=30},
+                new Workout{ID= 5, Instructor="Babs", Title = "XCO", Location = "Room 2",StartTime = today.NextDayAt(DayOfWeek.Wednesday, 9, 15), EndTime = today.NextDayAt(DayOfWeek.Wednesday, 10, 15), CapacityLeft=25},
+                new Workout{ID= 6, Instructor="Emilio", Title = "B&K Training", Location = "Boxing Area",StartTime = today.NextDayAt(DayOfWeek.Wednesday, 10, 15), EndTime = today.NextDayAt(DayOfWeek.Wednesday, 11, 15), CapacityLeft= 16},
+                new Workout{ID= 7, Instructor="Eva", Title = "Callanetics", Location = "Room 1", StartTime = today.NextDayAt(DayOfWeek.Wednesday, 19, 15), EndTime = today.NextDayAt(DayOfWeek.Wednesday, 20, 0), CapacityLeft=35},
+                new Workout{ID= 8, Instructor="Jeroen", Title = "Spinning", Location = "Room 4", StartTime = today.NextDayAt(DayOfWeek.Thursday, 10, 15), EndTime = today.NextDayAt(DayOfWeek.Thursday, 11, 15), CapacityLeft=18},
+                new Workout{ID= 9, Instructor="Marcel", Title = "Vinyasa Yoga", Location ="Yoga studio", StartTime = today.NextDayAt(DayOfWeek.Thursday, 17, 15), EndTime = today.NextDayAt(DayOfWeek.Thursday, 18, 15), CapacityLeft=30},
+                new Workout{ID= 10, Instructor="Anke", Title = "TBW", Location = "Room 1", StartTime = today.NextDayAt(DayOfWeek.Friday, 10, 15), EndTime = today.NextDayAt(DayOfWeek.Friday, 11, 0), CapacityLeft=35},
+                new Workout{ID= 11, Instructor="Emilio", Title = "Shred and Burn", Location = "Room 2", StartTime = today.NextDayAt(DayOfWeek.Friday, 10, 30), EndTime = today.NextDayAt(DayOfWeek.Friday, 11, 15), CapacityLeft=12},
+                new Workout{ID= 12, Instructor="Mirjam", Title = "Cycle Interval", Location = "Cycle Area", StartTime = today.NextDayAt(DayOfWeek.Friday, 18, 15), EndTime = today.NextDayAt(DayOfWeek.Friday, 19, 15), CapacityLeft=8},
+                new Workout{ID= 13, Instructor="Mirjam", Title = "Spinning", Location = "Cycle Area", StartTime = today.NextDayAt(DayOfWeek.Saturday, 9, 15), EndTime = today.NextDayAt(DayOfWeek.Saturday, 10, 15), CapacityLeft=12},
+                new Workout{ID= 14, Instructor="Lonneke", Title = "SoulRide", Location = "Cycle Area", StartTime = today.NextDayAt(DayOfWeek.Sunday, 10, 15), EndTime = today.NextDayAt(DayOfWeek.Sunday, 11, 15), CapacityLeft=6},
             };
 
 
